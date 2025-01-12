960bd16336f198685729ce9004ad30778a5b67e6
Arnav Ramya Coushik <70681041+Junkpup@users.noreply.github.com>
2025-01-12 15:53:50 +0530

    Update x3_s21_test1.m

--- S3/Test 1/x3_s21_test1.m
+++ S3/Test 1/x3_s21_test1.m
@@ -3,7 +3,7 @@ freq = data(:, 1);
 S21 = data(:, 2);  
 
 figure;
-plot(freq, S21, 'Color', [0, 0.5, 0], 'LineWidth', 1.5); % Dark forest green color
+plot(freq, S21, 'Color', [0, 0.5, 0], 'LineWidth', 1.5); 
 grid on;
 
 xlabel('Frequency (GHz)', 'FontSize', 12);
