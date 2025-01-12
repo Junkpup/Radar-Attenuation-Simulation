91425c7a5260ab621df54f6667ffe4130e60619e
Arnav Ramya Coushik <70681041+Junkpup@users.noreply.github.com>
2025-01-12 15:58:30 +0530

    x3_s11_test4.m

---
+++ S3/Test 4/x3_s11_test4.m
@@ -0,0 +1,12 @@
+data = readmatrix('x3_s11_test4.csv');
+freq = data(:, 1); 
+S11 = data(:, 2);  
+
+figure;
+plot(freq, S11, 'Color', [0.8, 0.4, 0.3], 'LineWidth', 1.5);
+grid on;
+
+xlabel('Frequency (GHz)', 'FontSize', 12);
+ylabel('S_{11} (dB)', 'FontSize', 12);
+title('X3 Composition Test 4', 'FontSize', 14);
+legend('S_{11}', 'Location', 'best');
