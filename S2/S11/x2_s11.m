c935cc3fb078a71610af458c723cbdf77333ba6b
Arnav Ramya Coushik <70681041+Junkpup@users.noreply.github.com>
2025-01-12 15:46:05 +0530

    x2_s11.m

---
+++ S2/S11/x2_s11.m
@@ -0,0 +1,12 @@
+data = readmatrix('x2_s11.csv');
+freq = data(:, 1); 
+S11 = data(:, 2);  
+
+figure;
+plot(freq, S11, 'Color', [0.8, 0.4, 0.3], 'LineWidth', 1.5); % Dark forest green color
+grid on;
+
+xlabel('Frequency (GHz)', 'FontSize', 12);
+ylabel('S_{11} (dB)', 'FontSize', 12);
+title('X2 Composition', 'FontSize', 14);
+legend('S_{11}', 'Location', 'best');
