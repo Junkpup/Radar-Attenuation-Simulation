be8ffbdaee51859640005d33d5d3d7796fec3918
Arnav Ramya Coushik <70681041+Junkpup@users.noreply.github.com>
2025-01-12 15:52:10 +0530

    x3_s21_test1.m

---
+++ S3/Test 1/x3_s21_test1.m
@@ -0,0 +1,12 @@
+data = readmatrix('x3_s21_test1.csv');
+freq = data(:, 1); 
+S21 = data(:, 2);  
+
+figure;
+plot(freq, S21, 'Color', [0, 0.5, 0], 'LineWidth', 1.5); % Dark forest green color
+grid on;
+
+xlabel('Frequency (GHz)', 'FontSize', 12);
+ylabel('S_{21} (dB)', 'FontSize', 12);
+title('X3 Composition Test 1', 'FontSize', 14);
+legend('S_{21}', 'Location', 'best');
